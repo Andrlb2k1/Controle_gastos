25b40dc133366b74f87ecc6abb51862451d8c504
Andrlb2k1 <86635909+Andrlb2k1@users.noreply.github.com>
2024-09-26 19:00:38 -0300

    register user

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Controle_gastos
-Criando uma aplicação completa de controle de gastos (não abra no Firefox)
+Criando uma aplicação completa de controle de gastos (não abra no Firefox). Possui alguns bugs.
---
+++ firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyBy-PZqQ02BxkT6oMcYnV4GlfKGuT9TK0M",
+	authDomain: "controle-de-gastos-9a0b2.firebaseapp.com",
+	projectId: "controle-de-gastos-9a0b2",
+	storageBucket: "controle-de-gastos-9a0b2.appspot.com",
+	messagingSenderId: "5789989666",
+	appId: "1:5789989666:web:456d29734d98a82e68c37b"
+};
+firebase.initializeApp(firebaseConfig);
