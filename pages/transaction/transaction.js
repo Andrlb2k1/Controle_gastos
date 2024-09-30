5aa81d5b963eae7c7468bc9cb8946a3f789859e6
Andrlb2k1 <86635909+Andrlb2k1@users.noreply.github.com>
2024-09-30 16:20:58 -0300

    save transaction

--- pages/transaction/transaction.js
+++ pages/transaction/transaction.js
@@ -1,3 +1,37 @@
+function saveTransaction() {
+    showLoading();
+
+    const transaction = createTransaction();
+
+    firebase.firestore()
+        .collection('transactions')
+        .add(transaction)
+        .then(() => {
+            hideLoading();
+            window.location.href = "../home/home.html";
+        })
+        .catch(() => {
+            hideLoading();
+            alert('Erro ao salvar transação')
+        })
+}
+
+function createTransaction() {
+    return {
+        type: form.typeExpense().checked ? "expense" : "income",
+        date: form.date().value,
+        money: {
+            currency: form.currency().value,
+            value: parseFloat(form.value().value)
+        },
+        transactionType: form.transactionType().value,
+        description: form.description().value,
+        user: {
+            uid: firebase.auth().currentUser.uid
+        }
+    }
+}
+
 function onChangeDate() {
     const date = form.date().value;
     form.dateRequiredError().style.display = !date ? "block" : "none";
@@ -46,11 +80,14 @@ function isFormValid() {
 }
 
 const form = {
+    currency: () => document.getElementById('currency'),
     date: () => document.getElementById('date'),
+    description: () => document.getElementById('description'),
     dateRequiredError: () => document.getElementById('date-required-error'),
     saveButton: () => document.getElementById('save-button'),
     transactionType: () => document.getElementById('transaction-type'),
     transactionTypeRequiredError: () => document.getElementById('transaction-type-required-error'),
+    typeExpense: () => document.getElementById('expense'),
     value: () => document.getElementById('value'),
     valueRequiredError: () => document.getElementById('value-required-error'),
     valueLessOrEqualToZeroError: () => document.getElementById('value-less-or-equal-to-zero-error')
