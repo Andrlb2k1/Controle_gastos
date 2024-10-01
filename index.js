80fc106e3aadfdac62c2379a24f256a64c1506c5
Andrlb2k1 <86635909+Andrlb2k1@users.noreply.github.com>
2024-10-01 15:39:52 -0300

    reverse node js

--- index.js
+++ index.js
@@ -93,16 +93,3 @@ const form = {
     passwordRequiredError: () => document.getElementById("password-required-error"),
     recoverPasswordButton: () => document.getElementById("recover-password-button"),
 }
-
-/*import express from 'express';
-
-// REST API http://api.controle-de-gastos.com/transactions
-const app = express();
-
-// GET      http://api.controle-de-gastos.com/transactions
-app.get('/transactions', (request, response) => {
-    console.log('GET transactions');
-    response.json([{id: 1}]);
-})
-
-app.listen(3000, () => console.log('API rest iniciada em http://localhost:3000'))*/
